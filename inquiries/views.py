03e8b058a8980f8c53532912c2ceee29b8e3d815
sh981013s <sh981013s@gmail.com>
2021-08-02 00:39:40 +0900

    feat: inquiries_api 추가

--- inquiries/views.py
+++ inquiries/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
 from config.views import BaseView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
 
 from .models import Inquiry
 from .serializers import InquirySerializer
+from .permissions import IsInquiryEditableOrDestroyable
+
 
 
 class InquiryListView(BaseView, ListAPIView):
@@ -16,3 +21,47 @@ class InquiryListView(BaseView, ListAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+class InquiryCreateView(BaseView, CreateAPIView):
+    """# InquiryCreateView
+    - 제보(place)를 신고한다 
+    """
+    serializer_class = InquirySerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.current_user) 
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class InquiryUpdateView(BaseView, UpdateAPIView):
+    """# InquiryUpdateView
+    - 신고(Inquiry)를 변경한다. (put/patch)
+    """
+    queryset = Inquiry.objects.all()
+    serializer_class = InquirySerializer
+    permission_classes = [IsAuthenticated, IsInquiryEditableOrDestroyable]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+
+
+class InquiryDestroyView(BaseView, DestroyAPIView):
+    """# InquiryDestroyView
+    - 유저의 신고를 취소한다.
+    """
+    queryset = Inquiry.objects.all()
+
+    serializer_class = InquirySerializer
+    permission_classes = [IsAuthenticated, IsInquiryEditableOrDestroyable]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
