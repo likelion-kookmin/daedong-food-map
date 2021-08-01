4b8d8d2c9a39351696aae4624941af91c3096667
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-01 18:04:56 +0900

    docs: 주석 추가

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -9,6 +9,12 @@ from .serializers import ReportSerializer
 
 
 class ReportListView(BaseView, ListAPIView):
+    """# ReportListView
+    - query params:
+      - user_id
+        - user_id가 있는 경우, 해당 유저가 제보한 목록이 반환된다.
+        - 없는 경우, 전체 제보 목록이 반환된다.
+    """
     serializer_class = ReportSerializer
 
     def get_queryset(self):
