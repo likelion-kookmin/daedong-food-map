d2a512eb34cbbd745df5836100760019e7df2293
sh981013s <sh981013s@gmail.com>
2021-08-02 00:39:40 +0900

    feat: inquiries_api 추가

--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import InquiryListView
+from .views import InquiryListView, InquiryCreateView, InquiryUpdateView, InquiryDestroyView
 
 app_name = 'inquiries'
 
 
 urlpatterns = [
     path('', InquiryListView.as_view()),
+    path('new/', InquiryCreateView.as_view()),
+    path('<int:pk>/edit/', InquiryUpdateView.as_view()),
+    path('<int:pk>/destroy/', InquiryDestroyView.as_view()),
 ]
