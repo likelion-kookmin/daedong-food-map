3b00b1a112d405cf391cf39b8b635dac63a339c8
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-02 00:00:19 +0900

    feat: reviews edit api 추가

---
+++ backend/reviews/permissions.py
@@ -0,0 +1,13 @@
+"""# reviews permissions"""
+from rest_framework import permissions
+
+
+class IsReviewEditableOrDestroyable(permissions.BasePermission):
+    """## IsReviewEditableOrDestroyable"""
+
+    def has_object_permission(self, request, view, obj):
+        """### has_object_permission"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user and (obj.user == request.user or request.user.is_staff)
