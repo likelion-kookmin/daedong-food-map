16d2515622808a18406e415355994147d4e22b0b
minjj0905 <minjj0905@kookmin.ac.kr>
2021-08-02 15:12:55 +0900

    chore: 앱 초기 세팅 및 디렉토리 구조 설정 - eslint/prettier 관련 설정 적용 - styled-component와 semantic-ui 등 관련 라이브러리 미리 적용, util 함수 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'styles.css';
+
+import Header from 'components/Header/header';
+import Footer from 'components/Footer/footer';
+import { Home } from 'pages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/pages/Home/home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Home() {
+  return (
+    <Fragment>
+      <img src="images/LogoTitle.png" style={{ width: '500px' }} alt="Logo" />
+      <p>Hello World!</p>
+    </Fragment>
+  );
+}
+
+export default Home;
