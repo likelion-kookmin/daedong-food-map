71bec3fb298e33c3dfd15ca3bb5259c3c7178e32
sh981013s <sh981013s@gmail.com>
2021-08-04 01:45:14 +0900

    feat: place에 user 값 연결

--- places/serializers.py
+++ places/serializers.py
@@ -1,8 +1,12 @@
 """# places serializers"""
 from file_managers.serializers import ImageSerializer
+from reports.models import Report
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from taggit_serializer.serializers import (TaggitSerializer,
                                            TagListSerializerField)
+from users.models import User
+from users.serializers import UserSerializer
 
 from .models import Place
 
@@ -13,6 +17,7 @@ class PlaceSerializer(TaggitSerializer, ModelSerializer):
     """
     tags = TagListSerializerField(required=False)
     images = ImageSerializer(many=True, required=False)
+    user = serializers.SerializerMethodField()
 
     class Meta:
         """### PlaceSerializer.Meta"""
@@ -27,3 +32,13 @@ class PlaceSerializer(TaggitSerializer, ModelSerializer):
             'updated_at',
             'deleted_at',
         ]
+
+    def get_user(self, obj):
+        """### get_user
+            - 현재 제보의 user 값을 가져온다
+        """
+        user = Report.objects.filter(place=obj).first().user
+        user_serializers = UserSerializer(user)
+        # user_email = {} .email
+        return user_serializers.data
+
