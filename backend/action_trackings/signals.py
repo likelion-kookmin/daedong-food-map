6f98e76747023d696f2ebde7a2d2c5bf6344d512
sh981013s <sh981013s@gmail.com>
2021-08-05 00:27:01 +0900

    feat: 제보, 신고, 리뷰 철회 action_tracking  추가

--- backend/action_trackings/signals.py
+++ backend/action_trackings/signals.py
@@ -33,6 +33,30 @@ def create_place_action_tracking(sender, **kwargs):
     user.report_count += 1
     user.save()
 
+@receiver(post_save, sender=Report)
+def destroy_place_action_tracking(sender, **kwargs):
+    """ ## destroy_place_action_tracking
+        - place report 을 destroy 한 아후의 action을 trackin합니다.
+        - action tracking 모델에 로그를 남기고, user.report_count -= 1 을 합니다.
+    """
+    instance = kwargs["instance"]
+
+    user = instance.user
+
+    if kwargs["created"]:
+        return
+
+    ActionTracking.create_user_action_tracking(
+        user=user,
+        rule_name=Action.DESTROY_REPORT,
+        actionable=instance
+
+    )
+
+    user.report_count -= 1
+    user.save()
+
+
 @receiver(post_save, sender=Review)
 def create_review_action_tracking(sender, **kwargs):
     """ ## create_review_action_tracking
@@ -54,6 +78,29 @@ def create_review_action_tracking(sender, **kwargs):
     )
 
     user.review_count += 1
+    user.save()
+
+@receiver(post_save, sender=Review)
+def destroy_review_action_tracking(sender, **kwargs):
+    """ ## destroy_review_action_tracking
+        - review destroy 후의 action을 tracking합니다.
+        - action tracking 모델에 로그를 남기고, user.review_count -= 1 을 합니다.
+    """
+    instance = kwargs["instance"]
+
+    user = instance.user
+
+    if kwargs["created"]:
+        return
+
+    ActionTracking.create_user_action_tracking(
+        user=user,
+        rule_name=Action.DESTORY_REVIEW,
+        actionable=instance
+
+    )
+
+    user.review_count -= 1
     user.save()
 
 @receiver(post_save, sender=Inquiry)
@@ -79,6 +126,25 @@ def create_inquiry_action_tracking(sender, **kwargs):
     user.inquiry_count += 1
     user.save()
 
+@receiver(post_save, sender=Inquiry)
+def destroy_inquiry_action_tracking(sender, **kwargs):
+    """ ## destroy_inquiry_action_tracking
+        - inqury destroy 후의 action을 tracking합니다.
+        - action tracking 모델에 로그를 남기고, user.inquiry_count -= 1 을 합니다.
+    """
+    instance = kwargs["instance"]
 
+    user = instance.user
 
+    if kwargs["created"]:
+        return
 
+    ActionTracking.create_user_action_tracking(
+        user=user,
+        rule_name=Action.DESTROY_INQUIRY,
+        actionable=instance
+
+    )
+
+    user.inquiry_count -= 1
+    user.save()
