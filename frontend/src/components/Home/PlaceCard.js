ad8529ea2c7fd11bac9670deb216862b3fbdf6b4
minjj0905 <minjj0905@kookmin.ac.kr>
2021-08-05 14:01:42 +0900

    feat: place card view, place view 기본 grid 구성

---
+++ frontend/src/components/Home/PlaceCard.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Icon, Label } from 'semantic-ui-react';
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 1rem 1rem 0.5rem 1rem;
+  position: relative;
+  background-color: #ffffff;
+  border: 1px solid #a1a1a1;
+  border-radius: 10px;
+`;
+
+const Section = styled.div`
+  display: flex;
+  align-items: end;
+  margin-bottom: 1rem;
+`;
+
+const Name = styled.div`
+  font-family: 'NS-EB';
+  font-size: 1.5rem;
+  color: #0c43b6;
+`;
+
+const Text = styled.div`
+  font-family: 'NS-R';
+  font-size: 1rem;
+  color: #707070;
+`;
+
+const Reviews = styled(Text)`
+  margin-left: 1rem;
+`;
+
+const Tag = styled(Label)`
+  font-family: 'NS-B';
+`;
+
+const Number = styled(Label)`
+  position: absolute;
+  top: 4%;
+  right: 2%;
+  font-family: 'NS-EB';
+`;
+
+const ImgContainer = styled.div`
+  position: relative;
+  flex-grow: 1;
+  padding-bottom: 30%;
+`;
+
+const Img = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  padding-right: 0.3rem;
+`;
+
+function PlaceCard(props) {
+  const imglist = props.data.pics.map((img, index) => (
+    <ImgContainer>
+      {index === 0 ? (
+        <Img src={img} style={{ borderRadius: '10px 0 0 10px' }} />
+      ) : index === props.data.pics.length - 1 ? (
+        <Img src={img} style={{ borderRadius: '0 10px 10px 0', padding: '0' }} />
+      ) : (
+        <Img src={img} />
+      )}
+    </ImgContainer>
+  ));
+
+  return (
+    <Container>
+      <Number circular size="large">
+        1
+      </Number>
+      <Section style={{ alignItems: 'baseline' }}>
+        <Name>{props.data.name}</Name>
+        <Icon name="star" style={{ color: '#F25C69', marginLeft: '0.8rem' }} />
+        <Text>{props.data.score}</Text>
+        <Reviews>리뷰 {props.data.reviews}개</Reviews>
+      </Section>
+      <Section style={{ justifyContent: 'space-between' }}>
+        <Section style={{ margin: '0' }}>
+          {props.data.tags.map((tag) => {
+            return <Tag>{tag}</Tag>;
+          })}
+        </Section>
+        <Text style={{ paddingTop: '0.5rem' }}>1.57km</Text>
+      </Section>
+      <Section style={{ justifyContent: 'space-between' }}>{imglist}</Section>
+    </Container>
+  );
+}
+
+export default PlaceCard;
--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { LOAD_PLACES_REQUEST, LOAD_PLACE_REQUEST } from 'reducers/place';
+import PlaceCard from 'components/Home/PlaceCard';
+import { Grid } from 'semantic-ui-react';
 
 function Home() {
   const dispatch = useDispatch();
@@ -20,7 +22,34 @@ function Home() {
     });
   }, [dispatch]);
 
-  return <Fragment>메인화면</Fragment>;
+  const testprops = {
+    id: 1,
+    name: '황금붕어빵',
+    score: '4.12',
+    reviews: '57',
+    tags: ['붕어빵', '국화빵', '호떡'],
+    pics: [
+      'https://newsimg.hankookilbo.com/cms/articlerelease/2020/12/22/d5b17b0b-690b-4b02-990f-2e0ad40d70da.jpg',
+      'https://i.ytimg.com/vi/khp4qDp3qB8/maxresdefault.jpg',
+      'https://hng.yna.co.kr/media/content/5694/1574845281067804.jpeg',
+    ],
+  };
+
+  return (
+    <Fragment>
+      <Grid stackable columns={2} paded>
+        <Grid.Column>
+          <PlaceCard data={testprops} />
+        </Grid.Column>
+        <Grid.Column>
+          <PlaceCard data={testprops} />
+        </Grid.Column>
+        <Grid.Column>
+          <PlaceCard data={testprops} />
+        </Grid.Column>
+      </Grid>
+    </Fragment>
+  );
 }
 
 export default Home;
