40c695e6349e5e7f1dc7fc2b623cc2a4577a1dfa
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-05 22:28:31 +0900

    chore: env 가져오는 방식 변경 및 배포 관련 문서 추가

--- backend/wait_for_postgres.py
+++ backend/wait_for_postgres.py
@@ -1,8 +1,8 @@
 import logging
 import os
 from time import sleep, time
-import dotenv
 
+import dotenv
 import psycopg2
 
 
@@ -15,24 +15,27 @@ def pg_isready(host, user, password, dbname, port):
             return True
         except psycopg2.OperationalError as e:
             logger.error(e)
-            logger.info(f"Postgres isn't ready. Waiting for {check_interval} {interval_unit}...")
+            logger.info(
+                f"Postgres isn't ready. Waiting for {check_interval} {interval_unit}...")
             sleep(check_interval)
 
-    logger.error(f"We could not connect to Postgres within {check_timeout} seconds.")
+    logger.error(
+        f"We could not connect to Postgres within {check_timeout} seconds.")
     return False
+
 
 if __name__ == "__main__":
     dotenv.read_dotenv()
     global check_timeout, check_interval
-    check_timeout = int(os.environ.get("POSTGRES_CHECK_TIMEOUT"))
-    check_interval = int(os.environ.get("POSTGRES_CHECK_INTERVAL"))
+    check_timeout = int(os. getenv("POSTGRES_CHECK_TIMEOUT"))
+    check_interval = int(os. getenv("POSTGRES_CHECK_INTERVAL"))
     interval_unit = "second" if check_interval == 1 else "seconds"
     config = {
-        "dbname": os.environ.get("POSTGRES_DB"),
-        "user": os.environ.get("POSTGRES_USER"),
-        "password": os.environ.get("POSTGRES_PASSWORD"),
-        "host": os.environ.get("DATABASE_URL"),
-        "port": os.environ.get("POSTGRES_PORT")
+        "dbname": os. getenv("POSTGRES_DB"),
+        "user": os. getenv("POSTGRES_USER"),
+        "password": os. getenv("POSTGRES_PASSWORD"),
+        "host": os. getenv("DATABASE_URL"),
+        "port": os. getenv("POSTGRES_PORT")
     }
 
     start_time = time()
