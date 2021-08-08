1c832fdf2eb0fb913e194ee7b6247d71f353b82e
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-08 22:37:44 +0900

    feat: report#new api 연결

--- frontend/src/sagas/report.js
+++ frontend/src/sagas/report.js
@@ -2,7 +2,7 @@ import { all, fork, put, takeLatest, call } from 'redux-saga/effects';
 import authHeader from './auth-header';
 import axios from 'axios';
 
-import { LOAD_REPORTS_REQUEST, LOAD_REPORTS_SUCCESS, LOAD_REPORTS_FAILURE } from 'reducers/report';
+import { LOAD_REPORTS_REQUEST, LOAD_REPORTS_SUCCESS, LOAD_REPORTS_FAILURE, ADD_REPORT_REQUEST, ADD_REPORT_SUCCESS, ADD_REPORT_FAILURE } from 'reducers/report';
 
 const reportListAPI = (data) => axios.get('/reports/', { headers: authHeader() }, data);
 
@@ -22,10 +22,32 @@ function* reportList(action) {
   }
 }
 
+const reportNewAPI = (data) => axios.post('/reports/new/', { headers: authHeader() }, data);
+
+function* reportNew(action) {
+  try {
+    const result = yield call(reportNewAPI, action.data);
+    yield put({
+      type: ADD_REPORT_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.log(err);
+    yield put({
+      type: ADD_REPORT_FAILURE,
+      error: err.respose.data,
+    });
+  }
+}
+
 function* watchReportList() {
   yield takeLatest(LOAD_REPORTS_REQUEST, reportList);
 }
 
+function* watchReportNew() {
+  yield takeLatest(ADD_REPORT_REQUEST, reportNew);
+}
+
 export default function* reportSaga() {
-  yield all([fork(watchReportList)]);
+  yield all([fork(watchReportList), fork(watchReportNew)]);
 }
