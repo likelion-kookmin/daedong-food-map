cfe7207e6d4820434065f330b49916573a4ce8ce
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-08 20:14:56 +0900

    feat: 신고 목록 추가

--- frontend/src/pages/Inquiry/inquiryList.page.jsx
+++ frontend/src/pages/Inquiry/inquiryList.page.jsx
@@ -1,20 +1,40 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
+import { LOAD_INQUIRIES_REQUEST } from 'reducers/inquiry';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { Icon, Label } from 'semantic-ui-react';
+import { Divider, Segment, Dimmer, Loader } from 'semantic-ui-react';
+
+import InquiryCard from 'components/Inquiry/inquiryCard';
 
 const InquiryListPage = () => {
   const dispatch = useDispatch();
+  const { inquiries, loadInquiriesLoading } = useSelector((state) => state.inquiry);
 
-  // useEffect(() => {
-  //   dispatch({
-  //     type: LOAD_PLACE_REQUEST,
-  //     id: id,
-  //   });
-  // }, [dispatch, id]);
+  useEffect(() => {
+    dispatch({
+      type: LOAD_INQUIRIES_REQUEST,
+    });
+  }, [dispatch]);
 
-  return <div>inquiry</div>;
+  return (
+    <div>
+      <h1>신고 목록</h1>
+      <Divider />
+      {loadInquiriesLoading ? (
+        <Segment>
+          <Dimmer active inverted>
+            <Loader size="huge">Loading</Loader>
+          </Dimmer>
+        </Segment>
+      ) : (
+        <Segment.Group>
+          {inquiries.map((inquiry) => (
+            <InquiryCard report={inquiry} />
+          ))}
+        </Segment.Group>
+      )}
+    </div>
+  );
 };
 
 export default InquiryListPage;
