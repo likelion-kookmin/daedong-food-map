7450b65970af0347537b43807bbb673dc57d8039
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-08 20:04:53 +0900

    feat: report 목록 페이지 기본 내용 추가

--- frontend/src/pages/Report/reportList.page.jsx
+++ frontend/src/pages/Report/reportList.page.jsx
@@ -1,20 +1,40 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
+import { LOAD_REPORTS_REQUEST } from 'reducers/report';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { Icon, Label } from 'semantic-ui-react';
+import { Divider, Segment, Dimmer, Loader } from 'semantic-ui-react';
+
+import ReportCard from 'components/Report/reportCard';
 
 const ReportListPage = () => {
   const dispatch = useDispatch();
+  const { reports, loadReportsLoading } = useSelector((state) => state.report);
 
-  // useEffect(() => {
-  //   dispatch({
-  //     type: LOAD_PLACE_REQUEST,
-  //     id: id,
-  //   });
-  // }, [dispatch, id]);
+  useEffect(() => {
+    dispatch({
+      type: LOAD_REPORTS_REQUEST,
+    });
+  }, [dispatch]);
 
-  return <div>report</div>;
+  return (
+    <div>
+      <h1>제보 목록</h1>
+      <Divider />
+      {loadReportsLoading ? (
+        <Segment>
+          <Dimmer active inverted>
+            <Loader size="huge">Loading</Loader>
+          </Dimmer>
+        </Segment>
+      ) : (
+        <Segment.Group>
+          {reports.map((report) => (
+            <ReportCard report={report} />
+          ))}
+        </Segment.Group>
+      )}
+    </div>
+  );
 };
 
 export default ReportListPage;
