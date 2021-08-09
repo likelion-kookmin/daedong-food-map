216681924c96b06f172bbe41f145f98c4772906f
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-10 00:26:21 +0900

    fix: current_user의 report만 보이도록 수정

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -21,14 +21,12 @@ class ReportListView(BaseView, ListAPIView):
         - 없는 경우, 전체 제보 목록이 반환된다.
     """
     serializer_class = ReportSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
     queryset = Report.objects.all()
 
     def get_queryset(self):
-        user_id = self.request.query_params.get('user_id')
-        if user_id:
-            return Report.objects.filter(user__id=user_id)
-        else:
-            return Report.objects.all()
+        return Report.objects.filter(user=self.current_user)
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
