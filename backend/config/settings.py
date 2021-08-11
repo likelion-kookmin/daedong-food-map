e8fe5bfaf8caea02a23d467f8bd91037ebf9eeb8
shinkeonkim <singun11@kookmin.ac.kr>
2021-08-11 23:10:25 +0900

    feat: CORS settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -239,6 +239,7 @@ EXPLORER_SCHEMA_INCLUDE_VIEWS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://daedong-food-map.netlify.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
